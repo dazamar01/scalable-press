1e2edfe6a2f9ca6be9123c5814d0a85f3e03dcb3
dazamar01 <dazamar01@hotmail.com>
2019-08-30 00:18:41 -0500

    saving progress...

--- business-time/business-time.js
+++ business-time/business-time.js
@@ -8,25 +8,46 @@ function convertToDate(dateTime) {
   return new Date(dateTime.getFullYear(),dateTime.getMonth() , dateTime.getDate());
 }
 
+// REFACTOR AT THE END
+// time = time
 function addBusinessTime(holiday, time, duration) {
-  let businessTimeDate;
+  let fc, spendedSeconds, pendingSeconds,  businessTimeDate = null;
 
-  duration = duration * 1000
-  
-  if ( convertToDate(time) >= convertToDate(holiday.start) && convertToDate(time) <= convertToDate(holiday.end)  ){
-    if (duration < 0){
-      businessTimeDate = new Date( holiday.start.getTime() + duration);
+  duration = duration * 1000;
+
+  fc = new Date(time.getTime() + duration);
+
+  if (time <= holiday.start ){
+    if ( fc <= holiday.start ){
+      businessTimeDate = new Date(fc);
     }else{
-      businessTimeDate = new Date( holiday.end.getTime() + duration);
+      if ( fc > holiday.start ){
+        spendedSeconds = (holiday.start.getTime() - time.getTime());
+        pendingSeconds = duration - spendedSeconds;
+        businessTimeDate = new Date(holiday.end.getTime() + pendingSeconds);
+      }
     }
   }else{
-    // console.log('fisrt case', time);
-    businessTimeDate = new Date( time.getTime() + duration );
+    if ( fc <= holiday.end ){
+      if (duration >= 0){
+        businessTimeDate = new Date ( holiday.end.getTime() + duration );
+      }else{
+        businessTimeDate = new Date ( holiday.start.getTime() + duration );
+      }
+    }else{
+      if ( time >= holiday.end ){
+        businessTimeDate = new Date(fc);
+      }else{
+        spendedSeconds = fc - holiday.end;
+        pendingSeconds = duration - spendedSeconds;
+        fc = holiday.end.getTime() + pendingSeconds;
+        businessTimeDate = new Date ( fc );
+      }
+    }
   }
-  
+    
   // Returns a Date object
   return businessTimeDate;
-  // return '--------';
 }
 
 const holiday = {
@@ -36,17 +57,19 @@ const holiday = {
 
 console.log(
   addBusinessTime(holiday, new Date('2019-12-01T00:00:00'), 60 * 60) // returns 2019-12-01T01:00:00
-)
+);
 
 console.log(
   addBusinessTime(holiday, new Date('2019-12-24T21:00:00'), 1) // returns 2019-12-25T21:00:01
-)
+);
 console.log(
   addBusinessTime(holiday, new Date('2019-12-24T20:30:00'), 60 * 60) // returns 2019-12-25T21:30:00
-)
+);
+
 console.log(
   addBusinessTime(holiday, new Date('2019-12-25T00:00:00'), 1) // returns 2019-12-25T21:00:01
-)
+);
+
 console.log(
   addBusinessTime(holiday, new Date('2019-12-25T00:00:00'), -1) // returns 2019-12-24T20:59:59
-)
+);
